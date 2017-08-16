cd127254cd545e81f574ff60874855f207bccc9d
caoyongren <m18410261910@163.com>
2017-08-16 09:25:48 +0800

    5.1

---
+++ Ch02/2_13-14_CustomTouch/app/src/main/java/com/a2_13_14_customtouch/DelegateActivity.java
@@ -0,0 +1,15 @@
+package com.a2_13_14_customtouch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DelegateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TouchDelegateLayout layout = new TouchDelegateLayout(this);
+
+        setContentView(layout);
+    }
+}
